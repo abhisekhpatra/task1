28c54a04b8cc9ca1ebea5258f118975138d497bf
abhisekhpatra <abhisekhpatra@gmail.com>
2017-03-25 11:13:25 -0700

    storing the recevied data'

--- TypeFormRequest/TypeFormRequest.py
+++ TypeFormRequest/TypeFormRequest.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 
 
 class TFR:
@@ -11,4 +12,39 @@ class TFR:
         print(api_url)
         response=requests.get(api_url)
         resposne_status=response.status_code
-        print(resposne_status)
+
+        if resposne_status == 404:
+             print("404 Error")
+        elif resposne_status == 403:
+            print("403 ERROR")
+
+        else: return response.json();
+
+
+    def get_data(selfself,API_KEY,id):
+        api_url="https://api.typeform.com/v1/forms/"+id+"?key="+API_KEY
+        response=requests.get(api_url)
+        resposne_status=response.status_code
+
+        if resposne_status == 404:
+             print("404 Error")
+        elif resposne_status == 403:
+            print("403 ERROR")
+
+        else: json=response.json()
+
+        response_data={}
+        response_data=json["fields"]
+
+        print(response_data.keys())
+
+        for i in response_data:
+            print(i)
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from TypeFormRequest import TFR
 import sys
 
 def main():
-
+    """ create object of TFR  """
     tfr=TFR()
-    tfr.get_form(sys.argv[1])
+
+    """ call get form fucntion of TFR and store the json data """
+
+    jsonData=tfr.get_form(sys.argv[1])
+
+    tfr.get_data(sys.argv[1],jsonData[0]['id'])
+
+
 
 
 
